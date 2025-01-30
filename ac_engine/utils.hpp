4f5afc6a66f405d184d40a1d1a826bcd395ef7f2
Arthur Carrell <Arthurcarrell08@gmail.com>
2025-01-30 15:41:55 +0000

    created a utils header file, and set up a bunch of stuff for the snake in the background.

---
+++ ac_engine/utils.hpp
@@ -0,0 +1,9 @@
+#ifndef AC_UTILS
+#define AC_UTILS
+
+struct vec2 { int x; int y; };
+struct vec2d { double x; double y; };
+
+struct color { int R; int G; int B; int A; };
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 /*
 
-SDL3 First test.
-This is the first time ive ever used SDL3. or really SDL in general unless you count one failed project.
-
-This is the code that im """borrowing""" from.
-https://examples.libsdl.org/SDL3/renderer/01-clear/
+Game engine that uses pixels
 
 */
 #define SDL_MAIN_USE_CALLBACKS 1 /* run SDL_AppInit instead of main() */
@@ -16,9 +12,11 @@ https://examples.libsdl.org/SDL3/renderer/01-clear/
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <list>
 
 #include <SDL3/SDL_stdinc.h>
 #include <SDL3/SDL_timer.h>
+#include "ac_engine/utils.hpp"
 #include <iostream>
 
 
@@ -26,14 +24,76 @@ using namespace std;
 
 
 const char PROJECT_Version[] = "1.0"; // project version; self explanatory.
-const char PROJECT_ProjectName[] = "com.asteristired.sdl3_test"; // the internal name of the project, no spaces or special characters.
-const char PROJECT_AppName[] = "SDL3 Test!"; // the name that appears on the window.
+const char PROJECT_ProjectName[] = "com.asteristired.snake"; // the internal name of the project, no spaces or special characters.
+const char PROJECT_AppName[] = "Snake"; // the name that appears on the window.
 
 const int WINDOW_Width = 1280;
 const int WINDOW_Height = 720;
 
+/* how many spaces there are in the game. DOES NOT represent the size of the window */
+const int GAME_MAX_X = 100; 
+const int GAME_MAX_Y = 100;
+
+const int PIXELS_PER_CELL = 1; // if this number goes below 1, stuff breaks real bad.
+
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+
+class Snake {
+    public:
+        vec2 position;
+        list<vec2> previous_positions;
+
+};
+
+class Apple {
+    public:
+        vec2 position;
+};
+
+Snake createSnake() {
+    Snake snake;
+    snake.position = {0, 0};
+    return snake;
+}
+
+Snake snake = createSnake();
+Apple apple;
+
+const color SNAKE_BODY = {74, 208, 26, SDL_ALPHA_OPAQUE};
+const color BACKGROUND_COLOR = {0,0,0,SDL_ALPHA_OPAQUE};
+/*
+===========================================================================================================
+                                                SNAKE GAME
+===========================================================================================================
+*/
+
+/* Runs on program start */
+void SnakeGame_Init() {
+    return;
+}
+
+/* Runs on key press/mouse click */
+void SnakeGame_AppEvent() {
+    return;
+}
+
+/* Runs each frame */
+void SnakeGame_Iterate() {
+    // set the background color to BACKGROUND.
+    SDL_SetRenderDrawColor(renderer, BACKGROUND_COLOR.R, BACKGROUND_COLOR.G, BACKGROUND_COLOR.B, BACKGROUND_COLOR.A);
+
+    /* clear the window to the draw colour */
+    SDL_RenderClear(renderer);
+    
+    /* place the new render onto the screen */
+    SDL_RenderPresent(renderer);
+    return;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 /* Runs on Startup */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
@@ -60,46 +120,32 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
     }
 
+    SnakeGame_Init();
     // everything has been properly intialised. continue with the program.
     return SDL_APP_CONTINUE;
 }
 
 /* runs on I/O interrupt. This could be a keypress or a mouse click. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    SnakeGame_AppEvent();
     if (event->type == SDL_EVENT_QUIT) {
         // a key that means quit the program has been pressed. (my example: SUPER+SHIFT+Q)
         return SDL_APP_SUCCESS; // quit the program, but in an intentional way, we are not quitting due to an error.
     }
+
     return SDL_APP_CONTINUE; // key was not a quit key, ignore and continue.
 }
 
-
-
 /* Runs once per frame, the meat and potatoes of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    const double now = ((double)SDL_GetTicks()) / 1000.0; // convert from miliseconds to seconds.
-    /*
-    A sine wave is used in order to smoothly flow through the colours.
-    This uses floating points to represent colours, however im gonna change that at some point to use RGBA.
-    */
-    const float red = (float) (0.5 + 0.5 * SDL_sin(now));
-    const float green = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const float blue = (float) (0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-
-    // set the render draw colour to the floating point value, being fully opaque. 
-    // TODO: Change this to use RGBA instead of floating points.
-    SDL_SetRenderDrawColorFloat(renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
-
-    /* clear the window to the draw colour */
-    SDL_RenderClear(renderer);
-    
-    /* place the new render onto the screen */
-    SDL_RenderPresent(renderer);
-
     // success, continue the program.
+    SnakeGame_Iterate();
     return SDL_APP_CONTINUE;
 
 }
+
+
+
 
 /* Runs on shutdown */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
